6c72144c208959c6f2a47de9100d48906128eaed
Daniel <dgplays14@gmail.com>
2022-09-29 07:09:55 -0300

    feat: add style variables

---
+++ src/styles/variables.ts
@@ -0,0 +1,25 @@
+export const colors = {
+	// Primary
+	moderateBlue: '#5457b6',
+	softRed: '#ed6468',
+	lightGrayishBlue: '#c3c4ef',
+	paleRed: '#ffb8bb',
+
+	// Neutral
+	darkBlue: '#324152',
+	grayishBlue: '#67727e',
+	lightGray: '#eaecf1',
+	veryLightGray: '#f5f6fa',
+	white: '#ffffff',
+}
+
+export const font = {
+	size: {
+		body: '1.6rem',
+	},
+	weight: {
+		regular: 400,
+		medium: 500,
+		bold: 700,
+	},
+}
